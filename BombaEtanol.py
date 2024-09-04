03b36544f2f29db588bad67faad098d1c89b9e1f
Rennan Rodrigues <rennan0rei@gmail.com>
2024-09-04 17:36:03 -0300

    Class BombaEtanol

--- BombaEtanol.py
+++ BombaEtanol.py
@@ -3,3 +3,4 @@ from bomba_combustivel import BombaCombustivel
 class BombaEtanol(BombaCombustivel):
     def _init_(self, valor_litro, quantidade_disponivel):
         super()._init_(valor_litro, quantidade_disponivel)
+        
