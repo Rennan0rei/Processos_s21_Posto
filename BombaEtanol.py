471153cd520bcff671be5922856e49530baaee71
Guilherme Henrique <guilhermehcma@gmail.com>
2024-09-04 18:08:20 -0300

    Paste name

--- BombaEtanol.py
+++
@@ -1,5 +0,0 @@
-from bomba_combustivel import BombaCombustivel
-
-class BombaEtanol(BombaCombustivel):
-    def _init_(self, valor_litro, quantidade_disponivel):
-        super()._init_(valor_litro, quantidade_disponivel)
